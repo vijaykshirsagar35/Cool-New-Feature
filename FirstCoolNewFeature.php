7a0f4770ba19b9334cb988d9d9da3867af9152e2
vijaykshirsagar35 <vijaykshirsagar35@gmail.com>
2020-05-14 10:43:14 +0530

    FirstFeature

---
+++ FirstCoolNewFeature.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vkshirsagar
+ * Date: 5/14/2020
+ * Time: 10:37 AM
+ */
+
+class CFirstCoolNewFeature {
+
+	public function printEcho() {
+		echo 'FirstCoolNewFeature';
+	}
+
+}
+
+$objNewFeature = new CFirstCoolNewFeature();
+$objNewFeature->printEcho();
